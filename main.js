55506b4a948aa5390e6d5b737873abcc58570443
mypieinteractive <54682130+mypieinteractive@users.noreply.github.com>
2026-03-11 15:16:37 -0600

    Update dashboard version and adjust imports

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // *
-// * Dashboard - V6.5
+// * Dashboard - V6.6
 // * FILE: main.js
 // * Description: Application entry point, global module binder for HTML templates.
 // *
 
-import { State, undoLastAction } from './state.js';
+import { State } from './state.js';
 import * as MapCtrl from './map.js';
 import * as UI from './ui.js';
 import * as API from './api.js';
@@ -25,7 +25,7 @@ window.setRoutes = UI.setRoutes;
 window.liveClusterUpdate = DragDrop.liveClusterUpdate;
 window.filterList = UI.filterList;
 window.setDisplayMode = UI.setDisplayMode;
-window.undoLastAction = undoLastAction;
+window.undoLastAction = UI.undoLastAction;
 window.toggleSelectAll = UI.toggleSelectAll;
 window.sortTable = UI.sortTable;
 window.handleInspectorChange = API.handleInspectorChange;
@@ -35,7 +35,7 @@ window.setNavPref = UI.setNavPref;
 window.handleEndpointInput = UI.handleEndpointInput;
 window.handleEndpointKeyDown = UI.handleEndpointKeyDown;
 window.handleEndpointBlur = UI.handleEndpointBlur;
-window.handleEndpointOptimize = UI.handleEndpointOptimize;
+window.handleEndpointOptimize = API.handleEndpointOptimize;
 window.checkEndpointModified = UI.checkEndpointModified;
 
 // Event Listeners for Mapbox Lasso Tool & Resize Handlers
